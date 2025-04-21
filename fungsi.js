eb1d10f7b580ae6b9baab4e74a0ad2fefcce0d19
Fajrul Syamsi <grandsmd@gmail.com>
2025-04-21 10:35:26 +0800

    menambahkan fungsi bagi

--- fungsi.js
+++ fungsi.js
@@ -1,4 +1,4 @@
-console.log('fungsi pretama');
+
 
 const add = (a, b) => a + b;
 console.log(add(11,2));
@@ -8,3 +8,7 @@ console.log(subtract(5,2));
 
 const multiply = (a,b)=> a * b;
 console.log(multiply(2,2));
+
+const divide = (a,b )=> a / b;
+console.log(divide(4,2));
+
